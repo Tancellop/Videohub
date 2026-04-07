d34e2139c9ed6f4397b9ee31fcaf7e7c23e85804
Tancellop <cheatertoplol12122@gmail.com>
2026-04-07 21:39:41 +0300

    comment

--- run.py
+++ run.py
@@ -1,7 +1,11 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 from app import create_app
 
-app = create_app(os.environ.get('FLASK_ENV', 'development'))
+app = create_app(os.environ.get('FLASK_ENV', 'production'))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
